8d1ac755f5e26e3e9764845ebaf4f590be7f4e2c
KarwaN001 <karwanusf1@gmail.com>
2025-09-02 09:11:33 -0700

    Add RxSwift and RxCocoa imports to ViewController.swift

--- pokemon-game/ViewController.swift
+++ pokemon-game/ViewController.swift
@@ -6,9 +6,14 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
+    private let disposeBag = DisposeBag()
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
